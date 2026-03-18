3fb9a2352a4a4908e2aa511e67f3bb22d2e77ba6
Kittyhgyu <suly.d.b.m@gmail.com>
2026-03-17 19:33:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -529,15 +529,3 @@ return phoneUtil.isValidNumber(parsedNumber)
 } catch (error) {
 return false
 }}
-
-import express from 'express'
-
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('Bot funcionando ✅')
-})
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('Servidor web activo en puerto', PORT)
-})
